817e5a10b107ef476d5dfe1dae3c7c2e55c0f5e0
Rahul Panchal <rm5901960@gmail.com>
2024-08-29 17:30:23 +0530

    undefined pf

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -145,7 +145,7 @@ const Nav = () => {
                   onClick={() => setIsOpen(false)}
                   className="flex flex-row w-full justify-between items-center w-full text-left py-2 text-sm hover:bg-gray-100"
                 >
-                  <img className="rounded-full w-6 h-6 border-3 border-white border-solid mr-2 object-cover" src={getCookie('socio-pf')?`https://lh3.googleusercontent.com/d/${getCookie('socio-pf')}`:"/d-prof.jpg"} alt="Profile" />
+                  <img className="rounded-full w-6 h-6 border-3 border-white border-solid mr-2 object-cover" src={getCookie('socio-pf') && getCookie('socio-pf')!="undefined"?`https://lh3.googleusercontent.com/d/${getCookie('socio-pf')}`:"/d-prof.jpg"} alt="Profile" />
                 </button>
               </Link>
               <div className="dot"></div>
@@ -198,7 +198,7 @@ const Nav = () => {
                 <button onClick={handleToggle} className="flex items-center">
                   {auth ? (
                     <div className="flex flex-row w-20 justify-evenly items-center ">
-                      <img className="rounded-full w-8 h-8 border-3 border-white border-solid mr-2 object-cover" src={getCookie('socio-pf')?`https://lh3.googleusercontent.com/d/${getCookie('socio-pf')}`:"/d-prof.jpg"} alt="Profile" />
+                      <img className="rounded-full w-8 h-8 border-3 border-white border-solid mr-2 object-cover" src={getCookie('socio-pf') && getCookie('socio-pf')!="undefined"?`https://lh3.googleusercontent.com/d/${getCookie('socio-pf')}`:"/d-prof.jpg"} alt="Profile" />
                       {getCookie("socio-user")}
                     </div>
                   ) : (
