f93f740d484191d075a6f945e189938fc5fdef6d
Rahul Panchal <rm5901960@gmail.com>
2024-09-01 21:35:33 +0530

    Info page updates

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,7 +1,7 @@
-import { AiBrain04Icon, Github01Icon } from "hugeicons-react"
+import { AiBrain04Icon, ArrowDiagonalIcon, Github01Icon } from "hugeicons-react"
 import "./info.css"
 import { BiRightArrow, BiSolidHeart } from "react-icons/bi"
-import { FaGithub } from "react-icons/fa"
+import { FaBoxOpen, FaGithub } from "react-icons/fa"
 import Nav from "../Navbar/Nav"
 
 const Info = ()=>{
@@ -9,7 +9,7 @@ const Info = ()=>{
         <div className="w-screen lg:mt-5 mt-5 overflow-y-scroll absolute top-0 right-0 flex flex-col justify-center items-center ">
             <Nav/>
             <h1 className="text-6xl lg:mb-10 mb-20 mt-24">About Socio</h1>
-            <div className="w-full px-5 lg:p-0 lg:w-1/2 text-xs flex flex-col justify-center items-top my-10">
+            <div className="w-full px-10 lg:p-0 lg:w-1/2 text-xs flex flex-col justify-center items-top my-10">
                 <p className="w-full text-sm flex flex-row justify-center items-center">
                     - In simple words, Socio is an AI powered social media platform. 
                     <br/>
@@ -20,16 +20,16 @@ const Info = ()=>{
                 </p>
             </div>  
             {/* <h2 className="mb-5"></h2> */}
-            <h1 className="text-2xl mb-5">Curious about how this works?</h1>
+            <h1 className="text-2xl mb-5">Curious about how it works?</h1>
             <ul className="w-1/2 list-disc" >
                 <li className="flex lg:flex-row flex-col w-full h-fit justify-center items-start py-2">
                     <b className="text-md lg:mt-0 mt-3">
                        AI Powered Categorization
                     </b>
                     <p className="text-5xl lg:mx-5 lg:my-0 mx-0 my-0 ">
-                        
+                        -
                     </p>
-                    <p className="text-sm"> 
+                    <p className="text-sm lg:mb-0 mb-10"> 
                         Gemini detects the Category or Emotion of the post being made and tags them with it, Which is used to identify user's interests.
                     </p>
                 </li>
@@ -40,7 +40,7 @@ const Info = ()=>{
                     <p className="text-5xl lg:mx-5 lg:my-0 mx-0 my-0 ">
                         -
                     </p>
-                    <p className="text-sm"> 
+                    <p className="text-sm lg:mb-0 mb-10"> 
                         Socio has a custom algorithm running on the server, powered by efficient MongoDB aggregation techniques and other data-structures. It basically sorts the feed based on user's likes map.
                     </p>
                 </li>
@@ -51,7 +51,7 @@ const Info = ()=>{
                     <p className="text-5xl lg:mx-5 lg:my-0 mx-0 my-0 ">
                         -
                     </p>
-                    <p className="text-sm"> 
+                    <p className="text-sm lg:mb-0 mb-10"> 
                         User can customie their profile. The profile is also tagged with the user's most liked category.
                     </p>
                 </li>
@@ -62,7 +62,7 @@ const Info = ()=>{
                     <p className="text-5xl lg:mx-5 lg:my-0 mx-0 my-0 ">
                         -
                     </p>
-                    <p className="text-sm"> 
+                    <p className="text-sm lg:mb-0 mb-10"> 
                         Based on the profile tags, all the people having common interests are returned first on the people section. The user will likely be friends with these chosen people.
                     </p>
                 </li>
@@ -73,7 +73,7 @@ const Info = ()=>{
                     <p className="text-5xl lg:mx-5 lg:my-0 mx-0 my-0 ">
                         -
                     </p>
-                    <p className="text-sm"> 
+                    <p className="text-sm lg:mb-0 mb-10"> 
                         The platform is connnected to a 15GB of google drive storage, and is capable of storing and delivering high quality data. For both Profile Pictures and Post Images.
                     </p>
                 </li>
@@ -84,7 +84,7 @@ const Info = ()=>{
                     <p className="text-5xl lg:mx-5 lg:my-0 mx-0 my-0 ">
                         -
                     </p>
-                    <p className="text-sm"> 
+                    <p className="text-sm lg:mb-0 mb-10"> 
                         Just like any other social media platform, Socio provides all the basic post actions such as Like posts and comments, Comment, Follow and Unfollow.
                     </p>
                 </li>
@@ -95,8 +95,24 @@ const Info = ()=>{
                     <p className="text-5xl lg:mx-5 lg:my-0 mx-0 my-0 ">
                         -
                     </p>
-                    <p className="text-sm"> 
+                    <p className="text-sm lg:mb-0 mb-10"> 
                         The user can traverse through the whole social network via post likes, comments, follower lists.
+                    </p>
+                </li>
+                <li className="flex lg:flex-row flex-col w-full h-fit justify-center items-start py-2">
+                    <b className="text-md lg:mt-0 mt-3">
+                        Open sourced
+                    </b>
+                    <p className="text-5xl lg:mx-5 lg:my-0 mx-0 my-0 ">
+                        -
+                    </p>
+                    <p className="text-sm lg:mb-0 mb-10"> 
+                        If you are that nerd who really wants to know how it works, do checkout the GitHub repositories. 
+                        <br />
+                        - <a className="text-blue-500" target="_blank" href="https://github.com/rahulpanchal0106/Socio-Web-Client">Front-end</a>
+                        <br />
+                        - <a className="text-blue-500" target="_blank" href="https://github.com/rahulpanchal0106/Socio-server">Back-end</a>
+
                     </p>
                 </li>
             </ul>
@@ -108,7 +124,7 @@ const Info = ()=>{
 
                 </p>
                 <div className="w-full text-xs flex flex-row justify-center items-center my-5">
-                    <a href="https://github.com/rahulpanchal0106/Socio-Web-Client"><FaGithub size={17}/></a>
+                    <a href="https://github.com/rahulpanchal0106/Socio-Web-Client" target="_blank"><FaGithub size={17}/></a>
                 </div>
             </div>  
         </div>
